7618c74389fdde3bfa24cd8dc610bff365adcc81
Anton Chernev <anton.chernev@paynetics.digital>
2026-03-16 17:08:36 +0200

    Initial Commit

---
+++ wake-up-neo/wake_up_neoView.h
@@ -0,0 +1,12 @@
+//
+//  wake_up_neoView.h
+//  wake-up-neo
+//
+//  Created by Anton Chernev on 16.03.26.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface wake_up_neoView : ScreenSaverView
+
+@end
---
+++ wake-up-neo/wake_up_neoView.m
@@ -0,0 +1,51 @@
+//
+//  wake_up_neoView.m
+//  wake-up-neo
+//
+//  Created by Anton Chernev on 16.03.26.
+//
+
+#import "wake_up_neoView.h"
+
+@implementation wake_up_neoView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
